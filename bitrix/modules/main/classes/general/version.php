f792f77d03169d09b8437ab6a1d180f102cfb7f4
dev4 <dev4@itg-soft.by>
2020-12-12 23:28:48 +0300

    sync

--- bitrix/modules/main/classes/general/version.php
+++ bitrix/modules/main/classes/general/version.php
@@ -1,3 +1,3 @@
 <?php
-define("SM_VERSION", "20.200.400");
-define("SM_VERSION_DATE", "2020-12-03 18:40:00"); // YYYY-MM-DD HH:MI:SS
+define("SM_VERSION", "20.200.500");
+define("SM_VERSION_DATE", "2020-12-11 09:59:44"); // YYYY-MM-DD HH:MI:SS
