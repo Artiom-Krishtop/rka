f522e0d21a7cddd76dd6f11adc83e2fe2cfbdc67
dev4 <dev4@itg-soft.by>
2021-05-17 15:25:43 +0300

    sync

--- bitrix/templates/cor/components/bitrix/news.list/news_front/template.php
+++ bitrix/templates/cor/components/bitrix/news.list/news_front/template.php
@@ -11,133 +11,73 @@
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
+$pev_txt = false;
 ?>
 <h2 class="block-title">Новости адвокатуры</h2>
-    
-  <div class="view view-news-on-mainpage view-id-news_on_mainpage view-display-id-block view-dom-id-02d3cd7afa0f44a2fd931e51cd0f005a">
-
-<div class="news-list view-content">
-<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?>
-<?endif;?>
-<ul class="lastnews">  
-<?foreach($arResult["ITEMS"] as $arItem):?>
-
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-    //$renderImage = CFile::ResizeImageGet($ar_res["PERSONAL_PHOTO"], Array("width" => 60, "height" => 60), BX_RESIZE_IMAGE_EXACT, false);
-    ?>
-	<li class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-		<?//if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<?//if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
- <span class="foto">   
-     <?if(!empty($arItem["PREVIEW_PICTURE"]["SRC"])){?>
-         <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
-                     class="preview_picture"
-                     border="0"
-                     src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-                     width="105"
-                     height="<?//=$arItem["DETAIL_PICTURE"]["HEIGHT"]?>"
-                     alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-                     title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-                     style="float:left"
-             /></a>
-     <?}elseif(!empty($arItem["DETAIL_PICTURE"]["SRC"])){?>
-         <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
-                     class="preview_picture"
-                     border="0"
-                     src="<?=$arItem["DETAIL_PICTURE"]["SRC"]?>"
-                     width="105"
-                     height="<?//=$arItem["DETAIL_PICTURE"]["HEIGHT"]?>"
-                     alt="<?=$arItem["DETAIL_PICTURE"]["ALT"]?>"
-                     title="<?=$arItem["DETAIL_PICTURE"]["TITLE"]?>"
-                     style="float:left"
-             /></a>
-     <?}else{?>
-
-     <?}?>
- </span>
-<span class="date">
-<?if(!empty($arItem['PROPERTIES']['PUBLIC_DATE']['VALUE'])){?>
-<?echo $arItem['PROPERTIES']['PUBLIC_DATE']['VALUE'] ?>
-<?}else{?>
-<?=FormatDate('d F Y',MakeTimeStamp($arItem['DISPLAY_ACTIVE_FROM']), time())?>
-<?
-
-//echo $arItem['DISPLAY_ACTIVE_FROM']?>
-<?}?>
-</span>                        
-			<?/*else:?>
-				<img
-					class="preview_picture"
-					border="0"
-					src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-					width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-					height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-					alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-					title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-					style="float:left"
-					/>
-			<?endif;*/?>
-		<?//endif?>
-		<?/*if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
-		<?endif*/?>
-		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a>
-			<?else:?>
-				<b><?echo $arItem["NAME"]?></b><br />
-			<?endif;?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<?echo $arItem["PREVIEW_TEXT"];?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<div style="clear:both"></div>
-		<?endif?>
-        <div>
-			<?//echo $arItem["PREVIEW_TEXT"];
-			
-			
-    $str = $arItem["~DETAIL_TEXT"];
-
-
-   // preg_match_all('/<p.*>([^\<]+)<\/p\s*>/i',$str,$matches);
-    //for inside html like a comment sais:
-    preg_match_all('/<p[^\>]*>(.*)<\/p\s*>/i',$str,$match);
-
-
-
-	echo($match[0][0]);	
-	//echo($match[0][1]);		
-			?>        
-        </div>
-		<?/*foreach($arItem["FIELDS"] as $code=>$value):?>
-			<small>
-			<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
-			</small><br />
-		<?endforeach;?>
-		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-			<small>
-			<?=$arProperty["NAME"]?>:&nbsp;
-			<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-				<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-			<?else:?>
-				<?=$arProperty["DISPLAY_VALUE"];?>
-			<?endif?>
-			</small><br />
-		<?endforeach;*/?>
-	</li>
-<?endforeach;?>
-</ul>
-<?/*if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;*/?>
-</div>
-<div class="more-link">
-  <a href="/news/">
-    Все новости адвокатуры  </a>
-</div>
+<div class="view view-news-on-mainpage view-id-news_on_mainpage view-display-id-block view-dom-id-02d3cd7afa0f44a2fd931e51cd0f005a">
+    <div class="news-list view-content">
+        <?if($arParams["DISPLAY_TOP_PAGER"]):?>
+            <?=$arResult["NAV_STRING"]?>
+        <?endif;?>
+        <ul class="lastnews">
+            <?foreach($arResult["ITEMS"] as $arItem):?>
+                <?
+                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                // preview image
+                $bImage = false;
+                if( !empty($arItem["PREVIEW_PICTURE"]["SRC"]) || !empty($arItem["DETAIL_PICTURE"]["SRC"]) )
+                {
+                    if (strlen($arItem["PREVIEW_PICTURE"]["SRC"]))
+                        $bImage = $arItem["PREVIEW_PICTURE"]["ID"];
+                    elseif (strlen($arItem["DETAIL_PICTURE"]["SRC"]))
+                        $bImage = $arItem["DETAIL_PICTURE"]["ID"];
+                }
+                $arImage = ($bImage ? CFile::ResizeImageGet($bImage, array('width' => 105, 'height' => 10000), BX_RESIZE_IMAGE_PROPORTIONAL_ALT, true) : array());
+                $imageSrc = ($bImage ? $arImage['src'] : '');
+                ?>
+                <li class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                    <?if($imageSrc):?>
+                        <span class="foto" style="float:left">
+                            <a href="<?=$arItem["DETAIL_PAGE_URL"]?>">
+                                <img class="preview_picture" src="<?=$imageSrc?>" alt="<?=($bImage ? $arItem['PREVIEW_PICTURE']['ALT'] : $arItem['NAME'])?>" title="<?=($bImage ? $arItem['PREVIEW_PICTURE']['TITLE'] : $arItem['NAME'])?>" />
+                            </a>
+                        </span>
+                    <?endif;?>
+                    <div class="body-info<?=($imageSrc ? ' with_img_left' : '');?>">
+                        <span class="date">
+                            <?
+                            if(!empty($arItem['PROPERTIES']['PUBLIC_DATE']['VALUE'])){
+                                echo $arItem['PROPERTIES']['PUBLIC_DATE']['VALUE'];
+                            }else{
+                                echo FormatDate('d F Y',MakeTimeStamp($arItem['DISPLAY_ACTIVE_FROM']), time());
+                            }
+                            ?>
+                        </span>
+                        <?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
+                            <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+                                <a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a>
+                            <?else:?>
+                                <b><?echo $arItem["NAME"]?></b><br />
+                            <?endif;?>
+                        <?endif;?>
+                        <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
+                            <?echo $arItem["PREVIEW_TEXT"];?>
+                        <?endif;?>
+                        <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+                            <div style="clear:both"></div>
+                        <?endif?>
+                        <?if(strlen(trim($arItem["~DETAIL_TEXT"])) && $pev_txt):?>
+                            <div><?
+                                $str = $arItem["~DETAIL_TEXT"];
+                                preg_match_all('/^<p[^>]*>.*?<\/p>/ims', $str, $match);
+                                echo($match[0][0]);
+                                ?></div>
+                        <?endif;?>
+                    </div>
+                </li>
+            <?endforeach;?>
+        </ul>
+    </div>
+    <div class="more-link"><a href="/news/">Все новости адвокатуры</a></div>
 </div>
